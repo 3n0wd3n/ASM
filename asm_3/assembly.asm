d9133ade461fa9521fe45aead93444d631d98bcc
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-03-16 18:45:40 +0100

    Add files via upload

--- asm_3/assembly.asm
+++ asm_3/assembly.asm
@@ -9,5 +9,18 @@ swap:
 	mov [rdi], ecx
 	ret
 
+iteration:
+	MOV eax, 1
+iteration_loop:
+	cmp eax, 6
+	je iteration_done
+	add eax, 1
+	jmp iteration_loop
+
+iteration_done:
+	ret
+
+
+
 
 
--- asm_3/main.c
+++ asm_3/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int swap(int *, int *);
-int iteration(int iter);
+// int iteration(int iter);
+int iteration();
 
 int main(int argc, char **argv)
 {
@@ -9,13 +10,12 @@ int main(int argc, char **argv)
 	int b = 24;
 	int *p1 = &a;
 	int *p2 = &b;
+	printf("Before swap %i %i \n", *p1, *p2);
 	swap(p1, p2);
-	printf("%i %i", *p1, *p2);
-	
-	
-	printf("Iteration to %i \n", iteration(5));
-	
-	
+	printf("After swap %i %i \n", *p1, *p2);
+
+	// printf("Iteration to %i \n", iteration(5));
+	printf("Iteration to %i \n", iteration());
+
 	return 0;
 }
-
