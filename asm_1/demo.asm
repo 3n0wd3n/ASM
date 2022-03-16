91f74f1cd51a30ac6031b0766fb788cedb18819d
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-03-16 20:12:22 +0100

    Add files via upload

--- asm_1/demo.asm
+++ asm_1/demo.asm
@@ -3,6 +3,7 @@ global circuit_of_square
 global content_of_square
 global sum
 global absi
+global content_of_rectangle
 
 section .text
 
@@ -10,22 +11,26 @@ demo:
 	mov eax, 42
 	ret
 
+
 circuit_of_square:
 	mov eax, edi
 	add eax, eax
 	add eax, eax
 	ret
 
+
 content_of_square:
 	mov eax, edi
-	imul eax, esi
+	imul eax, eax
 	ret
+
 
 sum:
 	mov eax, edi
 	mov ebx, esi
 	add eax, ebx
 	ret
+
 
 absi:
 	mov eax, edi
@@ -34,4 +39,11 @@ absi:
 	neg eax
 
 end:
+	ret
+
+
+content_of_rectangle:
+	mov eax, edi
+	mov ebx, esi
+	imul eax, ebx
 	ret
--- asm_1/main.c
+++ asm_1/main.c
@@ -2,15 +2,32 @@
 
 int demo(int n);
 int circuit_of_square(int n);
-int content_of_square(int a, int b);
+int content_of_square(int a);
 int sum(int n, int m);
 int absi(int l);
-
-int main(int n){
+int content_of_rectangle(int ab, int bc);
+int main(int n)
+{
         printf("Answer to life %i \n", demo(2));
-        printf("Circuit of square is %i \n", circuit_of_square(3));
-        printf("Content of square is %i \n", content_of_square(3, 5));
-        printf("Sum is %i \n", sum(2, 2));
-        printf("Abs value %i \n", absi(-7));
+
+        int edge = 3;
+        printf("Circuit of square with edge equals to %i is %i \n", edge, circuit_of_square(edge));
+        printf("Content of square with edge equals to %i is %i \n", edge, content_of_square(edge));
+
+        int a = -2;
+        int b = 3;
+        printf("Sum of %i and %i is  %i \n", a, b, sum(a, b));
+
+        int abs_n = -7;
+        printf("Abs value of number %i is %i \n", abs_n, absi(abs_n));
+
+        int ab = 3;
+        int bc = 6;
+        printf("Content of rectangle with one edge equals to %i and second one equals to %i is %i \n", ab, bc, content_of_rectangle(ab, bc));
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
         return 0;
 }
