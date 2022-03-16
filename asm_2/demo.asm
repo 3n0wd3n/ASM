83b8dea0d8d0e25d2181ea0b600cb646252b58ad
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-03-16 19:43:17 +0100

    Add files via upload

--- asm_2/demo.asm
+++ asm_2/demo.asm
@@ -3,12 +3,15 @@ global absolut_value
 global sgn
 global max2c
 global min3us
+global kladne
+global mocnina
 
 section .text
 
 demo:
         mov eax, 42
         ret
+
 
 absolut_value:
         mov eax, edi
@@ -18,6 +21,7 @@ absolut_value:
 
         end:
                 ret
+
 
 sgn:
         mov eax, edi
@@ -36,6 +40,7 @@ sgn:
                 mov eax, 1
                 ret
 
+
 max2c:
         mov eax, edi
         mov ebx, esi
@@ -47,8 +52,52 @@ max2c:
                 mov eax, ebx
                 ret
 
+
 min3us:
-        mov eax, edi
-        mov ebx, esi
-        mov ecx, r8d
+        mov eax, edx    ;do registru eax načtu první argument funkce
+        mov ebx, esi    ;do registru ebx načtu druhý argument funkce
+        mov ecx, edi    ;do registru ecx načtu tretí argument funkce
+comparison_loop:        
+        cmp eax, ebx
+        jle less_than
+        mov eax, ebx
+        jmp comparison_loop
+less_than:
+        cmp eax, ecx
+        jl most_less
+        mov eax, ecx
+        ret
+most_less:
+        ret
+
+
+kladne:
+        mov eax, edx   
+        mov ebx, esi    
+        mov ecx, edi
+        cmp eax, 0
+        jl zaporne
+        cmp ebx, 0
+        jl zaporne
+        cmp ecx, 0
+        jl zaporne
+        mov eax, 1
+        ret
+zaporne:
+        mov eax, 0
+        ret
+
+mocnina:
+        mov eax, edi    ;nahraju si číslo, které budu mocnit
+        mov ebx, esi    ;nahraju si číslo, které mi bude určovat kolikrat budu mocnit
+        mov edx, eax    ;udělam si kopii čísla, které se bude mocnit a pak ho využiju na násobení
+        mov ecx, 1      ;counter
+mocnina_loop:
+        cmp ecx, ebx
+        je end_iteration
+        add ecx, 1
+        imul eax, edx
+        jmp mocnina_loop
+
+end_iteration:
         ret
--- asm_2/main.c
+++ asm_2/main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+
 int demo(int n);
 int absolut_value(int a);
 int sgn(int i);
 char max2c(char a, char b);
 unsigned min3us(unsigned short a, unsigned short b, unsigned short c);
+int kladne(int a, int b, int c);
+int mocnina(int n, unsigned int m);
 
-int main(int n){
+int main()
+{
         printf("Answer to life %i \n", demo(2));
+
         printf("Absolut value %i \n", absolut_value(-7));
+
         int number = -4;
         printf("Signum of number %i is %i \n", number, sgn(number));
+
         char number_a = 9;
         char number_b = 6;
         printf("Maximal number from number a = %i and b = %i is number %i \n", number_a, number_b, max2c(number_a, number_b));
+
         unsigned short first_num = 9;
-        unsigned short second_num = 6;
+        unsigned short second_num = 3;
         unsigned short third_num = 1;
-        printf("Whis is small number from f_num = %i and s_num = %i and t_num = %i is %i \n", first_num, second_num, third_num, min3us(first_num, second_num, third_num));
+        printf("Which s is smallest number from f_num = %i and s_num = %i and t_num = %i is %i \n", first_num, second_num, third_num, min3us(first_num, second_num, third_num));
+
+        printf("If all values are greater than 0 function returns 1 otherwise 0: %i \n", kladne(-2, 3, 1));
+
+        printf("Function that returns n ** m: %i \n", mocnina(3, 4));
         return 0;
 }
