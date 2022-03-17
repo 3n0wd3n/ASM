2a71d5be4e1b9d1f3ba0ae98e528f5286a72a4a4
Mike <47132583+3n0wd3n@users.noreply.github.com>
2022-03-17 16:25:11 +0100

    ASM

--- asm_1/demo.asm
+++ asm_1/demo.asm
@@ -4,6 +4,11 @@ global content_of_square
 global sum
 global absi
 global content_of_rectangle
+global circuit_of_triangle
+global equilateral_triangle
+; global right_triangle
+global cube_volume
+global arithmetic_mean
 
 section .text
 
@@ -47,3 +52,46 @@ content_of_rectangle:
 	mov ebx, esi
 	imul eax, ebx
 	ret
+
+circuit_of_triangle:
+	mov eax, edx
+	mov ebx, edi
+	mov ecx, esi
+	add eax, ebx
+	add eax, ecx
+	ret
+
+equilateral_triangle:
+	mov eax, edi
+	imul eax, 3
+	ret
+
+; zeptat se proč to nejde 
+; right_triangle:
+; 	mov eax, edi
+; 	mov ebx, esi
+; 	mov ecx, 2
+; 	imul eax, ebx
+; 	div ecx
+; 	ret
+
+cube_volume:
+	mov eax, edi
+	mov ebx, eax
+	imul eax, eax
+	imul eax, ebx
+	ret
+
+; zeptat se učitele proč to nejde
+arithmetic_mean:
+	mov eax, edx
+	mov ebx, edi
+	mov ecx, esi
+	add eax, ebx
+	add eax, ecx
+	div 3
+	ret
+
+
+
+
--- asm_1/main.c
+++ asm_1/main.c
@@ -6,6 +6,12 @@ int content_of_square(int a);
 int sum(int n, int m);
 int absi(int l);
 int content_of_rectangle(int ab, int bc);
+int circuit_of_triangle(int d, int e, int f);
+int equilateral_triangle(int rt);
+// int right_triangle(int hanger_a, int hanger_b);
+int cube_volume(int cube_edge);
+unsigned int arithmetic_mean(int number_1, int number_2, int number_3);
+
 int main(int n)
 {
         printf("Answer to life %i \n", demo(2));
@@ -25,9 +31,24 @@ int main(int n)
         int bc = 6;
         printf("Content of rectangle with one edge equals to %i and second one equals to %i is %i \n", ab, bc, content_of_rectangle(ab, bc));
 
-        int a = 1;
-        int b = 2;
-        int c = 3;
+        int d = 1;
+        int e = 2;
+        int f = 3;
+        printf("Circuit of triangle with edges a = %i, b = %i and c = %i is %i \n", d, e, f, circuit_of_triangle(d, e, f));
 
+        int rt = 4;
+        printf("Circuit of equilateral triangle with edge equals to %i is %i \n", rt, equilateral_triangle(rt));
+
+        int hanger_a = 3;
+        int hanger_b = 4;
+        // printf("Content of right triangle with hanger one equals to %i and hanger two equals to %i is %i \n", hanger_a, hanger_b, right_triangle(hanger_a, hanger_b));
+
+        int cube_edge = 5;
+        printf("Volume of cube with edge equals to %i is %i \n", cube_edge, cube_volume(cube_edge));
+
+        unsigned int number_1 = 4;
+        unsigned int number_2 = 3;
+        unsigned int number_3 = 5;
+        printf("Arithmetic mean of numbers %i, %i, %i is %i", number_1, number_2, number_3, arithmetic_mean(number_1, number_2, number_3));
         return 0;
 }
