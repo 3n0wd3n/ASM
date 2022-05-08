425ae4139d0aea3d97a99912781e5a3fa0304132
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-05-08 20:21:33 +0200

    test

--- ASM-C-DOKUMENTY/zapoctova_uloha_2/druha_zapoctova_uloha(os1)/paruniq.c
+++ ASM-C-DOKUMENTY/zapoctova_uloha_2/druha_zapoctova_uloha(os1)/paruniq.c
@@ -36,6 +36,9 @@ int main(int argc, char *argv[])
     {
         // testing the argument of the function
         printf("%s\n", argv[1]);
+        char line[BUFSIZ];
+        fgets(line, sizeof(line), stdin);
+        printf("stdin: %s\n", line);
         printf("It lives\n");
         // thread_one stores the information about thread and its data typy is p_thread
         pthread_t thread_one, thread_two;
